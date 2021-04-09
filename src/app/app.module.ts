45efd5fbb873c30c28f2228f6ccdbe898b1aaa9d
cleyvison matos <cleyvison.matos@gmail.com>
2021-04-09 09:04:40 -0300

    compasso-desafio

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,22 @@
+
+import { AppRoutingModule } from './app-routing.module';
+import { CoreModule } from './core/core.module';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
 
+import { UsersModule } from './users/users.module';
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    CoreModule,
+    AppRoutingModule,
+    UsersModule,
+
+
   ],
   providers: [],
   bootstrap: [AppComponent]
